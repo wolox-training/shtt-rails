046ff0c8047b43d265545050b02f90f76e25fbab
Samir Tapiero <samir.hernando@wolox.co>
2019-05-13 15:32:46 -0500

    Remove user table

--- db/schema.rb
+++ db/schema.rb
@@ -26,32 +26,4 @@ ActiveRecord::Schema.define(version: 20190513183559) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "provider", default: "email", null: false
-    t.string "uid", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.boolean "allow_password_change", default: false
-    t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "current_sign_in_ip"
-    t.datetime "current_sign_in_at"
-    t.integer "sign_in_count"
-    t.string "last_sign_in_ip"
-    t.datetime "last_sign_in_at"
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.string "email"
-    t.json "tokens"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
-  end
-
 end
