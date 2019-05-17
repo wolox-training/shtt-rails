ab720447d9f703195427d8719c4290fcd42e9164
Samir Tapiero <samir.hernando@wolox.co>
2019-05-17 15:08:35 -0500

    Removed after actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include Wor::Paginate
   include Pundit
-  after_action :verify_authorized, except: :index
-  after_action :verify_policy_scoped, only: :index
 
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
