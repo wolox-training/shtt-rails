9d1a43c4a961bab12234c3f97542ccfade32599c
Samir Tapiero <samir.hernando@wolox.co>
2019-05-23 15:06:04 -0500

    Added limit in book title

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190517220222) do
+ActiveRecord::Schema.define(version: 20190523194609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20190517220222) do
     t.string "genre", null: false
     t.string "author", null: false
     t.string "image", null: false
-    t.string "title", null: false
+    t.string "title", limit: 25, null: false
     t.string "editor", null: false
     t.string "year", null: false
     t.datetime "created_at", null: false
