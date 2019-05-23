7faff23337bfe4974d635b3e23383894f31070f1
Samir Tapiero <samir.hernando@wolox.co>
2019-05-23 15:14:50 -0500

    Added limit in book factory

--- spec/factories/book.rb
+++ spec/factories/book.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     genre { Faker::Book.genre }
     author { Faker::Book.author }
     image { Faker::File.file_name('path/to') }
-    title { Faker::Book.title }
+    title { Faker::Book.title.slice(0, 25) }
     editor { Faker::Book.publisher }
     year { Faker::Number.number(4) }
   end
